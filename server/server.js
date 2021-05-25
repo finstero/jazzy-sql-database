75a46ba259cf402cf644a2830a7531da9c9b1c80
Olivia Finster <olivia.finster@gmail.com>
2021-05-25 16:50:51 -0500

    setup pool

--- server/server.js
+++ server/server.js
@@ -4,6 +4,26 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+const pg = require('pg');
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'localhost',
+    port: 5432
+});
+
+pool.on('connect', () =>{
+    console.log('CONNECTED TO POSTGRES');
+});
+
+pool.on('error', (error) => {
+    console.log(error);
+});
+
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
